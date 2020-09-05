a57355e57a01ac9d3135a342067b87d207cba825
Omkar Amilkanthwar <omkarsamilkanthwar2000@gmail.com>
2020-09-05 09:27:41 +0530

    Update api.py

--- api.py
+++ api.py
@@ -3,11 +3,17 @@ from flask import Flask, jsonify
 app = Flask(__name__)
 
 
-li = [{"model":"omkar", "age": 20},{"model":"modellll", "age": 20},{"model":"ultra modellll", "age": 20}]
+li = [{"model":"omkar", "age": 20},{"model":"modellll", "age": 20},{"model":"ultra modellll", "age": 20}]    # data you want to pass
 
-@app.route('/posts')
-def method():
-    return jsonify(li)
+@app.route('/posts')    # route path
+def method():           # method will be called for above route
+    return jsonify(li)  # will create json return..............
 
 if __name__ == "__main__":
-    app.run(host = '192.168.43.165')
+    app.run(host = '192.168.43.165')  # add ip address of your laptop         
+    
+    
+   
+# run this server first
+# install android app
+
